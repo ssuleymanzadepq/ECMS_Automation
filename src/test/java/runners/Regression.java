ce8ba751bd7cc26576ca5eac9d4a354159353e77
ssuleymanzadepq <samir.suleymanzade@proquest.com>
2022-04-25 12:15:24 -0400

    lc04252022

---
+++ src/test/java/runners/Regression.java
@@ -0,0 +1,21 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features/",
+        glue = "steps",
+        //when dry run set to true, it stops the actual execution and check for the step definition
+        dryRun = false,
+        monochrome = true,
+        tags = "@regression",
+        plugin = {"pretty", "html:target/cucumber.html",
+        "json:target/cucumber.json",
+        "rerun:target/failed.txt"}
+)
+
+public class Regression {
+}
